7cc52eeb078b681a4335b2ffa9f9279734039d6b
Pussy <drish2345@mail.ru>
2020-06-30 21:30:25 +0300

    123

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 '''qewrqsd - domain()'''
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
