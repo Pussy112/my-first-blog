021216e772993c9d67326c41c5c27da919489366
Pussy <drish2345@mail.ru>
2020-06-30 20:22:16 +0300

    My Django Girls app, first commit3

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def post_list(self):
-    return HttpResponse('hi bitch')
+def post_list(request):
+    return render(request, 'blog/post_list.html', ())
