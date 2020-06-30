ebb3f17ec70f738d094baea09e0dfdcf1edd3622
Pussy <drish2345@mail.ru>
2020-06-30 21:01:15 +0300

    My Django Girls app, first commit6

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-'''qewrqsd - domain'''
+'''qewrqsd - domain()'''
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
