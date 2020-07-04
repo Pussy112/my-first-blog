ad6fcb17d6c04fa0d0c0010ad9570edea0627ce6
Pussy <drish2345@mail.ru>
2020-07-04 12:33:07 +0300

    rec

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -11,9 +11,13 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[:4]
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
 
 def post_new(request):
     if request.method == "POST":
@@ -21,12 +25,13 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+    
+
     
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -35,9 +40,27 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
